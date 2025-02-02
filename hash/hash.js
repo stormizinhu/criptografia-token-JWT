833becfb98987e1dc3e9398d0c42363c18d89240
Stormizinhu <brun0_lp@hotmail.com>
2025-02-02 10:45:34 -0300

    Push

---
+++ hash/hash.js
@@ -0,0 +1,33 @@
+import { createHash } from 'crypto';
+
+function criaHash(senha) {
+    return createHash('sha256').update(senha).digest('hex');
+}
+
+console.log(criaHash("uma String Qualquer"));
+
+class Usuario {
+    constructor(nome, senha) {
+        this.nome = nome;
+        this.hash = criaHash(senha);
+    }
+
+    autentica(nome, senha){
+        if (nome === this.nome && this.hash === criaHash(senha)) {
+            console.log("Usuário autenticado com sucesso");
+            return true;
+        }
+
+        console.log("Usuário ou senha incorretos");
+        return false;
+    }
+}
+
+const usuario = new Usuario('joao manoel', 'minhaSenha');
+
+console.log(usuario)
+
+usuario.autentica('joao manoel', 'minhaSenha');
+
+usuario.autentica('jm', 'minhaSenha');
+usuario.autentica('joao manoel', 'minhasenha');
---
+++ token/token.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const chaveSecreta = "chaveSuperSecreta";
+
+const token = jwt.sign({
+    apelido: "jm",
+    curso: "segurança em node.js"
+    }, chaveSecreta
+)
+
+console.log(token)
+
+const tokenDecodificado = jwt.verify(token, chaveSecreta);
+
+console.log(tokenDecodificado)
